aec4c4b7d9cc09324edd88441b70e68083db25dc
Mario Torres <scaramuzzini@gmail.com>
2024-08-19 10:47:55 -0400

    First import

---
+++ app/src/main/java/com/oceanbrasil/android20240819/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.oceanbrasil.android20240819
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btEnviar = findViewById<Button>(R.id.button)
+        val tvMensagem = findViewById<TextView>(R.id.textViewOlaMundo)
+
+        btEnviar.setOnClickListener {
+            //trocar o texto
+            tvMensagem.text = "Novo texto..."
+        }
+
+
+    }
+}
