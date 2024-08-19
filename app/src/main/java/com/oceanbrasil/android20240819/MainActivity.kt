ccbb8a358d708ed9da2d4be30bf794b774c34ac5
Mario Torres <scaramuzzini@gmail.com>
2024-08-19 11:06:24 -0400

    LinearLayout adicionado

--- app/src/main/java/com/oceanbrasil/android20240819/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/android20240819/MainActivity.kt
@@ -13,15 +13,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(R.layout.activity_main_alternativo)
 
-        val btEnviar = findViewById<Button>(R.id.button)
-        val tvMensagem = findViewById<TextView>(R.id.textViewOlaMundo)
+        val btEnviar = findViewById<Button>(R.id.button2)
+        val tvMensagem = findViewById<TextView>(R.id.textView)
 
         btEnviar.setOnClickListener {
             //trocar o texto
