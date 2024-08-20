f9c3efa80a2f49196ff5009ce70cfebd899f8d04
Mario Torres <scaramuzzini@gmail.com>
2024-08-20 10:16:37 -0400

    Resultado Activity

--- app/src/main/java/com/oceanbrasil/android20240819/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/android20240819/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.oceanbrasil.android20240819
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +21,14 @@ class MainActivity : AppCompatActivity() {
         val btEnviar = findViewById<Button>(R.id.button2)
         val tvMensagem = findViewById<TextView>(R.id.textView)
 
+        val etNome = findViewById<EditText>(R.id.editTextText)
+        val tvNomeDigitado = findViewById<TextView>(R.id.tvNomeDigitado)
+
+        val novaTelaIntent = Intent(this, ResultadoActivity::class.java)
         btEnviar.setOnClickListener {
             //trocar o texto
-            tvMensagem.text = "Novo texto..."
+            tvNomeDigitado.text = etNome.text
+            startActivity(novaTelaIntent)
         }
 
 
